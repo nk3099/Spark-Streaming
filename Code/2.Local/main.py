9c68ee8761e806c7d71abc3653fc685c4eaebab8
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-03-14 20:47:07 +0530

    Add files via upload

---
+++ Code/2.Local/main.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode,split
+
+if __name__=='__main__':
+    print("creating spark session")
+
+    spark = SparkSession \
+    .builder \
+    .master("local[*]") \
+    .appName("Neeraj Spark Streaming") \
+    .config("spark.sql.shuffle.partitions","8") \
+    .config("spark.sql.adaptive.enabled", "false") \
+    .getOrCreate()
+
+
+#read the data
+    lines = spark.readStream.format("socket") \
+        .option("host","localhost") \
+        .option("port","9999") \
+        .load()
+    
+#processing logic
+    words = lines.select(explode(split(lines.value," ")).alias("word"))
+    wordCounts = words.groupBy("word").count()
+
+#writing to the sink
+    query = wordCounts.writeStream \
+        .format("console") \
+        .outputMode("complete") \
+        .option("checkpointLocation","checkpointdir1") \
+        .start()
+    
+    query.awaitTermination()
+
+    
